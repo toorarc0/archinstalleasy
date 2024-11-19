439b62dd35c32b01ba42b16df40785d523492807
TOORARC0 <toorarc0@gmail.com>
2024-11-19 19:46:07 +0500

    I will make new version
    AI help me

--- archinstallforsda.sh
+++ archinstallforsda.sh
@@ -17,84 +17,35 @@ parted $DISK mklabel gpt
 echo "Создание разделов..."
 
 # 50 ГБ для системы
-parted $DISK mkpart primary ext4 1MiB 50GiB
+parted $DISK mkpart primary ext4 1MiB 50GiB -1
 
 # 1 ГБ для EFI
-parted $DISK mkpart primary fat32 50GiB 51GiB
+parted $DISK mkpart primary fat32 50GiB 51GiB -1
 parted $DISK set 2 esp on
 
 # 4 ГБ для Swap
-parted $DISK mkpart primary linux-swap 51GiB 55GiB
+parted $DISK mkpart primary linux-swap 51GiB 55GiB -1
 
 # Оставшееся пространство для Home
-parted $DISK mkpart primary ext4 55GiB 100%
+parted $DISK mkpart primary ext4 55GiB 100% -1
 
 # Форматирование разделов
 echo "Форматирование разделов..."
 
 # Форматирование раздела для системы
-mkfs.ext4 ${DISK}1
+mkfs.ext4 -F ${DISK}1
 
 # Форматирование EFI-раздела
 mkfs.fat -F32 ${DISK}2
 
 # Форматирование Swap-раздела
-mkswap ${DISK}3
+mkswap -f ${DISK}3
 
 # Форматирование раздела для Home
-mkfs.ext4 ${DISK}4
+mkfs.ext4 -F ${DISK}4
 
 # Активация Swap
-swapon ${DISK}3
-
-echo "Форматирование завершено. Разделы созданы и отформатированы."
-
-# Убедитесь, что скрипт запускается с правами суперпользователя
-if [ "$EUID" -ne 0 ]; then
-    echo "Пожалуйста, запустите этот скрипт с правами суперпользователя (sudo)."
-    exit 1
-fi
-
-# Диск для форматирования
-DISK="/dev/sda"
-
-# Удаление всех существующих разделов на диске
-echo "Удаление всех существующих разделов на $DISK..."
-parted $DISK mklabel gpt
-
-# Создание разделов
-echo "Создание разделов..."
-
-# 50 ГБ для системы
-parted $DISK mkpart primary ext4 1MiB 50GiB
-
-# 1 ГБ для EFI
-parted $DISK mkpart primary fat32 50GiB 51GiB
-parted $DISK set 2 esp on
-
-# 4 ГБ для Swap
-parted $DISK mkpart primary linux-swap 51GiB 55GiB
-
-# Оставшееся пространство для Home
-parted $DISK mkpart primary ext4 55GiB 100%
-
-# Форматирование разделов
-echo "Форматирование разделов..."
-
-# Форматирование раздела для системы
-mkfs.ext4 ${DISK}1
-
-# Форматирование EFI-раздела
-mkfs.fat -F32 ${DISK}2
-
-# Форматирование Swap-раздела
-mkswap ${DISK}3
-
-# Форматирование раздела для Home
-mkfs.ext4 ${DISK}4
-
-# Активация Swap
-swapon ${DISK}3
+swapon -a ${DISK}3
 
 # Монтирование разделов
 echo "Монтирование разделов..."
@@ -118,29 +69,30 @@ echo "Система смонтирована в /mnt"
 echo "EFI смонтирован в /mnt/boot/efi"
 echo "Home смонтирован в /mnt/home"
 
-pacstrap -i /mnt base base-devel linux linux-firmware linux-headers sudo nano networkmanager efibootmgr
+pacstrap -i /mnt base base-devel linux linux-firmware linux-headers sudo nano networkmanager efibootmgr --noconfirm
 
-genfstab -U -p /mnt >> /mnt/etc/fstab
-arch-chroot /mnt /bin/bash
+genfstab -U -p /mnt >> /mnt/etc/fstab --noconfirm
 
-sed -i 's/#$en_US.UTF-8$/\1/' /etc/locale.gen
-sed -i 's/#$ru_RU.UTF-8$/\1/' /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-ln -sf /usr/share/zoneinfo/Asia/Yekaterinburg /etc/localtime
-hwclock --systohc --utc
-echo archpc > /etc/hostname
-echo "127.0.1.1 localhost.localdomain archpc" | sudo tee -a /etc/hosts
-systemctl enable NetworkManager
-useradd -m -g users -G wheel -s /bin/bash superuser
-passwd superuser
+arch-chroot /mnt /bin/bash --noconfirm
+
+sed -i 's/#$en_US.UTF-8$/en_US.UTF-8/' /etc/locale.gen
+sed -i 's/#$ru_RU.UTF-8$/ru_RU.UTF-8/' /etc/locale.gen
+locale-gen --noconfirm
+echo "LANG=en_US.UTF-8" > /etc/locale.conf --noconfirm
+ln -sf /usr/share/zoneinfo/Asia/Yekaterinburg /etc/localtime --noconfirm
+hwclock --systohc --utc --noconfirm
+echo archpc > /etc/hostname --noconfirm
+echo "127.0.1.1 localhost.localdomain archpc" | sudo tee -a /etc/hosts --noconfirm
+systemctl enable NetworkManager --noconfirm
+useradd -m -g users -G wheel -s /bin/bash superuser --noconfirm
+passwd superuser --noconfirm
 EDITOR=nano
-sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
-mkdir /boot/efi
-pacman -S grub
-grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi --removable
-grub-mkconfig -o /boot/grub/grub.cfg
-passwd
+sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers --noconfirm
+mkdir /boot/efi --noconfirm
+pacman -S grub --noconfirm
+grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi --removable --noconfirm
+grub-mkconfig -o /boot/grub/grub.cfg --noconfirm
+passwd --noconfirm
 exit
 umount -R /mnt
 reboot
